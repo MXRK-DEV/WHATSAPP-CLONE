c8580152eb18e572bd6a8e6dcac29cfc7568fe6c
mark <damark215@gmail.com>
2024-04-18 03:23:36 +0100

    Webhooks added

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://super-bluejay-23.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
