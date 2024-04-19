312958616bd8c3f98c36248304d0a56c393ee6c8
mark <damark215@gmail.com>
2024-04-19 15:25:28 +0100

    get group members

--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -3,17 +3,20 @@
 import { ListFilter, LogOut, MessageSquareDiff, Search, User } from "lucide-react";
 import { Input } from "../ui/input";
 import ThemeSwitch from "./theme-switch";
-import { conversations } from "@/dummy-data/db";
 import Conversation from "./conversation";
 import { SignedOut, UserButton } from "@clerk/nextjs";
 import { SignInButton, SignOutButton, SignedIn } from "@clerk/clerk-react";
 import UserListDialog from "./user-list-dialog";
-import { useConvexAuth } from "convex/react";
+import { useConvexAuth, useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 const LeftPanel = () => {
 	const {isAuthenticated} = useConvexAuth();
-	// const conversations = [];
+	const conversations = useQuery(api.conversations.getMyConversations, isAuthenticated ? undefined : "skip");
 
+	console.log(conversations);
+	// const conversations = [];
+     
 	return (
 		<div className='w-1/4 border-gray-600 border-r'>
 			<div className='sticky top-0 bg-left-panel z-10'>
@@ -54,7 +57,7 @@ const LeftPanel = () => {
 			{/* Chat List */}
 			<div className='my-3 flex flex-col gap-0 max-h-[80%] overflow-auto'>
 				{/* Conversations will go here*/}
-				{conversations.map((conversation) => (
+				{conversations?.map((conversation) => (
 					<Conversation key={conversation._id} conversation={conversation}/>
 				))}
 
