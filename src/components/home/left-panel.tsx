9ba3f294be8d9302530fbb6585b5bea360588104
mark <damark215@gmail.com>
2024-04-25 12:31:14 +0100

    kick user mutation added

--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -9,13 +9,24 @@ import { SignInButton, SignOutButton, SignedIn } from "@clerk/clerk-react";
 import UserListDialog from "./user-list-dialog";
 import { useConvexAuth, useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
+import { useConversationStore } from "@/store/chat-store";
+import { useEffect } from "react";
 
 const LeftPanel = () => {
-	const {isAuthenticated} = useConvexAuth();
+	const {isAuthenticated, isLoading} = useConvexAuth();
 	const conversations = useQuery(api.conversations.getMyConversations, isAuthenticated ? undefined : "skip");
 
-	console.log(conversations);
-	// const conversations = [];
+	const {selectedConversation, setSelectedConversation} = useConversationStore()
+
+	useEffect(() => {
+		const conversationIds = conversations?.map((conversation) => conversation._id);
+		if (selectedConversation && conversationIds && !conversationIds.includes(selectedConversation._id)) {
+			setSelectedConversation(null);
+		}
+	}, [conversations, selectedConversation, setSelectedConversation]);
+
+	if (isLoading) return null;
+
      
 	return (
 		<div className='w-1/4 border-gray-600 border-r'>
