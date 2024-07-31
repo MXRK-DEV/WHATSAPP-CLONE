42e79e9b2af42ebe85c081d9e05ca3426c5bc431
mark <damark215@gmail.com>
2024-07-31 21:40:15 +0100

    Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
     images:{
         remotePatterns: [
             { hostname: "strong-sandpiper-465.convex.cloud" }
